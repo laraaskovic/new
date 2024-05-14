6c5aaacc69a18f9d57e05833c3e4a9d4a59a9a54
laraaskovic <lara.askovic@gmail.com>
2024-05-14 13:11:28 -0400

    s

--- food/src/App.js
+++ food/src/App.js
@@ -5,23 +5,34 @@ import Montage from './components/Montage';
 
 const App = () => {
   const [photos, setPhotos] = useState([]);
+  const [photoCounter, setPhotoCounter] = useState(0);
 
   const addPhoto = (photoData) => {
     const date = new Date().toISOString().split('T')[0]; // Get current date
 
     setPhotos([...photos, photoData]);
+    setPhotoCounter(prevCounter => prevCounter + 1); // Increment photo counter
+    animateCounter(); // Trigger the animation
+  };
+
+  const animateCounter = () => {
+    const counterElement = document.querySelector('.photo-counter');
+    counterElement.classList.remove('counting');
+    void counterElement.offsetWidth; // Trigger reflow to restart animation
+    counterElement.classList.add('counting');
   };
 
   return (
     <div className="app">
       <header className="header">
-      <img src="/images/timewarp (1).png" alt="Logo" />
+        <img src="/images/timewarp (1).png" alt="Logo" />
       </header>
       <div className="main">
-        
         <Camera addPhoto={addPhoto} />
         <Montage photos={photos} />
-
+      </div>
+      <div className="footer">
+      
       </div>
     </div>
   );
