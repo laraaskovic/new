abce264f76aeac9900f90f59b3b3a5d7478ae100
laraaskovic <lara.askovic@gmail.com>
2025-11-01 18:02:32 -0400

    Update Camera.js

--- food/src/components/Camera.js
+++ food/src/components/Camera.js
@@ -14,6 +14,7 @@ const Camera = ({ addPhoto }) => {
   const [statusMessage, setStatusMessage] = useState('Initialising face aligner...');
   const [modelState, setModelState] = useState('loading');
 
+
   useEffect(() => {
     let cancelled = false;
 
